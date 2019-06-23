adafd7c4d0e8aa3b6ef96bccd8e620f7b7c2d373
Maulana Firdaus <firdausmaulan@gmail.com>
2019-06-23 15:26:29 +0700

    add feature news

--- app/src/main/java/com/firdaus/example/ui/news/NewsAdapter.kt
+++ app/src/main/java/com/firdaus/example/ui/news/NewsAdapter.kt
@@ -1,45 +1,80 @@
 package com.firdaus.example.ui.news
 
-import android.databinding.DataBindingUtil
+import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.firdaus.example.R
 import com.firdaus.example.databinding.AdapterNewsBinding
 import com.firdaus.example.model.news.Article
 
+
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-    private val listArticle = mutableListOf<Article>()
+    private val listArticle: MutableList<Article> = mutableListOf<Article>()
 
     fun update(list: List<Article>) {
         listArticle.addAll(list)
         notifyDataSetChanged()
+        Log.d("NewsAdapter", "list Size = ${list.size}")
+        Log.d("NewsAdapter", "listArticle Size = ${listArticle.size}")
+        Log.d("NewsAdapter", "itemCount = $itemCount")
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): NewsAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemBinding = AdapterNewsBinding.inflate(layoutInflater, parent, false)
+        Log.d("NewsAdapter", "onCreateViewHolder")
+        return ViewHolder(itemBinding)
+    }
+
+    inner class ViewHolder(private val binding: AdapterNewsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            Log.d("NewsAdapter", "bind")
+            binding.article = article
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("NewsAdapter", "onBindViewHolder")
+        val item = listArticle[position]
+        holder.bind(item)
+    }
+
+    /*override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): NewsAdapter.ViewHolder {
         return ViewHolder(viewGroup)
     }
 
     class ViewHolder(
-            private val parent: ViewGroup,
-            private val binding: AdapterNewsBinding = DataBindingUtil.inflate(
-                    LayoutInflater.from(parent.context),
-                    R.layout.adapter_news,
-                    parent,
-                    false
-            )
+        private val parent: ViewGroup,
+        private val binding: AdapterNewsBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.adapter_news,
+            parent,
+            false
+        )
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(article: Article) {
+        fun bind(article: Article?) {
             binding.article = article
         }
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         Log.d("NewsAdapter", "onBindViewHolder")
-        viewHolder.bind(listArticle[position])
+        if (listArticle.size > 0) viewHolder.bind(listArticle[position])
+    }*/
+
+    companion object {
+        @BindingAdapter("items")
+        @JvmStatic
+        fun RecyclerView.bindItems(items: List<Article>?) {
+            val adapter = adapter as NewsAdapter
+            if (!items.isNullOrEmpty()) adapter.update(items)
+        }
     }
 
-    override fun getItemCount() = listArticle.size
+    override fun getItemCount(): Int {
+        return listArticle.size
+    }
 }
--- app/src/main/java/com/firdaus/example/utils/ImageBinder.java
+++ app/src/main/java/com/firdaus/example/utils/ImageBinder.java
@@ -3,6 +3,7 @@ package com.firdaus.example.utils;
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public final class ImageBinder {
 
@@ -10,8 +11,10 @@ public final class ImageBinder {
         //NO-OP
     }
 
+    private static RequestOptions requestOptions = new RequestOptions().centerCrop();
+
     @BindingAdapter("imageUrl")
     public static void setImageUrl(ImageView imageView, String url) {
-        Glide.with(imageView).load(url).into(imageView);
+        Glide.with(imageView).load(url).apply(requestOptions).into(imageView);
     }
 }
